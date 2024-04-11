477016f8b8e47bf8c0cc2b0d6b16d39e4946491f
Yuvaraj-Rathod <yuvrajrathod0007@gmail.com>
2024-04-11 16:47:13 +0530

    on charcters and digits Authentication

--- app/src/main/java/com/example/practiceapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -33,11 +33,43 @@ class MainActivity : AppCompatActivity() {
                 val username  =userInput.text.toString()
                 val password  =passwordInput.text.toString()
 
-                if (username.length > 6 && password.length > 6) {
-                    Log.i("test credentials", "username:$username and password:$password")
-                } else {
-                    Toast.makeText(this, "Username and password must be more than 6 characters", Toast.LENGTH_SHORT).show()
+                loginButton.setOnClickListener {
+                    val username = userInput.text.toString()
+                    val password = passwordInput.text.toString()
+
+                    if (username.length > 6 && !username.containsDigits()) {
+                        if (password.length > 6 && password.containsDigits() && password.containsCharacters()) {
+                            Log.i("test credentials", "username:$username and password:$password")
+                        } else {
+                            when {
+                                password.length <= 6 -> {
+                                    Toast.makeText(this, "Password should contain more than six characters", Toast.LENGTH_SHORT).show()
+                                }
+                                !password.containsDigits() || !password.containsCharacters() -> {
+                                    Toast.makeText(this, "Password must contain both digits and characters", Toast.LENGTH_SHORT).show()
+                                }
+
+                            }
+                        }
+                    } else {
+                        when {
+                            username.length <= 6 -> {
+                                Toast.makeText(this, "Username should contain more than six characters", Toast.LENGTH_SHORT).show()
+                            }
+                            username.containsDigits() -> {
+                                Toast.makeText(this, "Username must contain only characters", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
                 }
-        }
+
+            }
     }
+}
+fun String.containsDigits(): Boolean {
+    return any { it.isDigit() }
+}
+
+fun String.containsCharacters():Boolean{
+    return any{it.isLetter()}
 }
