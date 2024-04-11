047bc5429533dc4c71c24d85fb46183ba8cd28fb
Yuvaraj-Rathod <yuvrajrathod0007@gmail.com>
2024-04-11 16:09:29 +0530

    on characters authentication

--- app/src/main/java/com/example/practiceapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.practiceapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var userInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+            userInput = findViewById(R.id.username_input)
+            passwordInput= findViewById(R.id.password_input)
+            loginButton= findViewById(R.id.login_btn)
+
+            loginButton.setOnClickListener{
+                val username  =userInput.text.toString()
+                val password  =passwordInput.text.toString()
+
+                if (username.length > 6 && password.length > 6) {
+                    Log.i("test credentials", "username:$username and password:$password")
+                } else {
+                    Toast.makeText(this, "Username and password must be more than 6 characters", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
